aec99118b9ea85b2ecc1478f70b46e5efd5ceaea
oscar <oscarzxdead@gmail.com>
2023-08-23 20:48:35 -0400

    first commit

---
+++ src/data.js
@@ -0,0 +1,44 @@
+export const data = [
+	{
+		id: 1,
+		img: 'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/f655b4fb-35e9-4a63-acf8-c02dfd78cfda/zoom-fly-5-road-running-shoes-lkx7Zp.png',
+		nameProduct: 'Nike rosados',
+		price: 80,
+		quantity: 1,
+	},
+	{
+		id: 2,
+		img: 'https://media.revistagq.com/photos/5eb513ee3f0f1d1f6dbca8b7/4:3/w_639,h_479,c_limit/air-jordan-1-white-royal-release-date.jpg',
+		nameProduct: 'Nike retro',
+		price: 50,
+		quantity: 1,
+	},
+	{
+		id: 3,
+		img: 'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/1c64a0b0-a0aa-46dd-98e7-7ab5bfc09211/sb-pogo-skate-shoes-MpL6DC.png',
+		nameProduct: 'Nike street',
+		price: 90,
+		quantity: 1,
+	},
+	{
+		id: 4,
+		img: 'https://ss201.liverpool.com.mx/xl/1064524527.jpg',
+		nameProduct: 'Gorra plana',
+		price: 50,
+		quantity: 1,
+	},
+	{
+		id: 5,
+		img: 'https://americasport.net/images/productos/Franela_Nike_Academy_Neon/eXCuh1b6Pd.jpg',
+		nameProduct: 'Camisa deportiva',
+		price: 20,
+		quantity: 1,
+	},
+	{
+		id: 5,
+		img: 'https://f.fcdn.app/imgs/7640a2/www.globalsports.com.uy/gls/e2c1/original/catalogo/NKDQ4728-010-1/2000-2000/short-nike-dri-fit-challenger-run-division-black.jpg',
+		nameProduct: 'shorts deportivos',
+		price: 20,
+		quantity: 1,
+	}
+];
